3070ab1077b0fa5b392aea3290b5dcbf15ba6090
Igor Malyarov <igor1309@gmail.com>
2020-09-08 13:48:53 +0300

    Initial Commit

---
+++ RFin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RFin
+//
+//  Created by Igor Malyarov on 08.09.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RFin/RFinApp.swift
@@ -0,0 +1,17 @@
+//
+//  RFinApp.swift
+//  RFin
+//
+//  Created by Igor Malyarov on 08.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct RFinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
